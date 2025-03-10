863d4838ce0fb3c2da9662bbd190e131f0345aec
charlie roberts <charlie@charlie-roberts.com>
2025-03-10 15:14:13 -0700

    Update main.mjs

--- main.mjs
+++ main.mjs
@@ -1,18 +1,18 @@
 const marching_blob = await fetch('https://cdn.jsdelivr.net/gh/charlieroberts/marching@main/dist/index.js')
-const marching_txt = await res.text()
-eval( marcing_txt )
+const marching_txt  = await marching_blob.text()
+eval( marching_txt )
 
 window.screamer = await import('https://cdn.jsdelivr.net/gh/charlieroberts/screamer@latest/screamer.js')
 window.screamer = screamer.default
 
 const screamerlang_blob = await fetch('https://cdn.jsdelivr.net/gh/charlieroberts/screamer@latest/screamer-lang.js')
-const screamerlang_txt = await screamerlang.text()
+const screamerlang_txt  = await screamerlang_blob.text()
 eval( screamerlang_txt )
 
 screamer.initHydra = function() {}
 screamer.init()
 
-const canvas        = document.createElement('canvas')
+const canvas  = document.createElement('canvas')
 canvas.width  = window.width
 canvas.height = window.height
 
